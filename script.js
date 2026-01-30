8afc20e730352d807a8092dd795373eff6de0051
m-j0401 <mehul.jain2206@gmail.com>
2026-01-30 13:11:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let list=document.querySelector("#band");
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+bands.sort((a,b)=>a.localeCompare(b));
+let fragment=document.createDocumentFragment();
+for(let b of bands)
+	{
+		let li=document.createElement("li");
+		li.innerText=b;
+		fragment.append(li);
+	}
+list.append(fragment);
+
+
